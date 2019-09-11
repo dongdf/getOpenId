d6d8993fe1cfed633c6adb8a7191def5cd9b342b
dongdf <fa.me@163.com>
2019-09-11 18:44:24 +0800

    1
    1

--- src/pages/mine/minfo.vue
+++ src/pages/mine/minfo.vue
@@ -2,13 +2,14 @@
     <div class="mbginfo">
       <div class="headertop">
       <div class="pinfoheader">
-        <img class="bgimg" src="../../assets/img/pbanner1.jpg"/>
+        <img class="bgimg" v-if="curInfo.role == 1" src="../../assets/img/pbanner2.jpg"/>
+        <img class="bgimg" v-else src="../../assets/img/pbanner1.jpg"/>
         <div class="pinfoContent">
            <div class="headerimg">
              <img  :src="curInfo.avatar?curInfo.avatar:morehead"/>
            </div>
            <div class="headerinfos">
-             <h3>{{curInfo.name?curInfo.name:curInfo.nickname}}<span>未签约</span></h3>
+             <h3>{{curInfo.name?curInfo.name:curInfo.nickname}}<span v-show="!curInfo.sign_status && curInfo.role!=0">未签约</span></h3>
              <p @click="gocompany()" v-show="allcList.length>1"><label class="micons"><img src="../../assets/img/storeicon1.png"/> </label> {{curInfo.company_name}}<label class="micons"><img class="downimg" src="../../assets/img/storedown.png"/> </label></p>
              <p v-show="allcList.length==1"><label class="micons"><img src="../../assets/img/storeicon1.png"/> </label> {{curInfo.company_name}}</p>
            </div>
@@ -33,7 +34,7 @@
 
       </div>
 
-      <div class="quickicon">
+      <div class="quickicon" @click="showtip">
         <div class="quickiconItem">
           <div class="iconimg"><img  src="../../assets/img/picon1_1.jpg"/></div>
           <p>优享+</p>
@@ -54,7 +55,7 @@
       </div>
       <div class="mlists">
 <!--用工宝-->
-        <ul>
+        <ul @click="showtip()" v-show="curInfo.role  == 1">
           <li>
             <div class="lname">我的工作</div>
             <div class="lbq"><span>3988</span><i class="aright"><img src="../../assets/img/arrowRight.jpg"/> </i></div>
@@ -67,7 +68,7 @@
 <!--用工宝-->
 
         <!--创业宝-->
-        <ul>
+        <ul  @click="showtip()" v-show="curInfo.role  == 2">
           <li>
             <div class="lname">我的个体工商执照</div>
             <div class="lbq"><i class="aright"><img src="../../assets/img/arrowRight.jpg"/> </i></div>
@@ -90,7 +91,7 @@
 
 <!--分工宝-->
 
-        <ul>
+        <ul  @click="showtip()" v-show="curInfo.role  == 3">
 
           <li>
             <div class="lname">我的业务承揽</div>
@@ -133,12 +134,15 @@
     },
     methods:{
       gopage(str){
-        this.$router.push({
-          path:'/mine',
-          query:{
-            funCode:str
-          }
-        })
+        if(this.checkcontract()){
+          this.$router.push({
+            path:'/mine',
+            query:{
+              funCode:str
+            }
+          })
+        }
+
       },
       gocompany(){
         this.$router.replace({
@@ -174,19 +178,19 @@
 
         })
       },
-      getCurinfo(id){
-        this.request.post('mapi/getOneCompanyInfo',{company_id:id}).then(res=>{
-          if(res.code == 0){
-            this.curInfo = res.data
-            console.log(this.curInfo)
-return false;
+      showtip(){
+        this.$messagebox.alert('功能开发中,敬请期待')
+      },
+      checkcontract(){
+        if(!this.curInfo.sign_status && this.curInfo.role !=0){
+          if(this.curInfo.role == 1){//用工宝
             this.$promot({
               name:'$promot',
               width:'80%',
               title:'基本信息',
               funCode:'qyts',
               props:{
-                isableclose:false,
+                isableclose:true,
                 tipText:'请签约合同用工宝合同'
 
               },
@@ -202,10 +206,74 @@ return false;
                 })
               }
             })
+            return false;
+          }else if(this.curInfo.role == 2){//创业宝宝
+            this.$promot({
+              name:'$promot',
+              width:'80%',
+              title:'基本信息',
+              funCode:'qyts',
+              props:{
+                isableclose:true,
+                tipText:'请签约合同用工宝合同'
+
+              },
+              callback:(close)=>{
+                close();
+                this.$router.push({
+                  path:'/authPerson',
+                  query:{
+                    funCode:'businessAuth',
+                    setp:2,
+                    cid:id
+                  }
+                })
+              }
+            })
+            return false;
+          }else if(this.curInfo.role == 3){//用工宝
+            this.$promot({
+              name:'$promot',
+              width:'80%',
+              title:'基本信息',
+              funCode:'qyts',
+              props:{
+                isableclose:true,
+                tipText:'请签约创业宝合同'
+
+              },
+              callback:(close)=>{
+                close();
+                this.$router.push({
+                  path:'/authPerson',
+                  query:{
+                    funCode:'personalAuth',
+                    setp:2,
+                    cid:id
+                  }
+                })
+              }
+            })
+            return false;
+          }else{
+            return true;
+          }
+
+
+
+        }
+      },
+      getCurinfo(id){
+        this.request.post('mapi/getOneCompanyInfo',{company_id:id}).then(res=>{
+          if(res.code == 0){
+            this.curInfo = res.data
+
+
+            this.checkcontract();
+            // this.$toast(res.msg)
 
           }else{
-            this.$toast(res.msg)
-
+            alert(res.msg)
           }
 
         },error=>{
