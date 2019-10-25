298b7285c21f4939b32167c842b647514ff06112
12366 <zhengwencheng@12366.com>
2019-10-25 09:25:41 +0800

    eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -21,8 +21,10 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'indent': [0, 2], // 强制缩进4 spaces
+    'handle-callback-err': 0,
     // allow async-await
-    'generator-star-spacing': 'off',
+    'generator-star-spacing': [2, 'both'], //生成器函数前后空格
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
