87a768c4f78b288a61288e762bc60d14cf235d6c
12366 <zhengwencheng@12366.com>
2019-10-25 16:43:55 +0800

    立即签约

--- src/pages/mine/qyts.vue
+++ src/pages/mine/qyts.vue
@@ -1,31 +1,38 @@
 <template>
-    <div class="qytscontent">
-      <img src="../../assets/img/qyts.jpg">
-      <p>{{tipText}}</p>
-      <div class="qbtn">
-        <span @click="callback()">立即签约</span>
-      </div>
+  <div class="qytscontent">
+    <img src="../../assets/img/qyts.jpg">
+    <p>{{tipText}}</p>
+    <div class="qbtn">
+      <span @click="callback()">立即签约</span>
     </div>
+  </div>
 </template>
 
 <script>
   export default {
-    props:['ableClose','tipText'],
-    name: "qyts",
-    created(){},
-    methods:{}
+    props: ['ableClose', 'tipText'],
+    name: 'qyts',
+    created () {
+    },
+    methods: {}
   }
 </script>
 
 <style lang="scss" scoped>
-  .qytscontent{
+  .qytscontent {
     text-align: center;
-    margin:0 auto;
-    p{font-size:35px; padding:0px 0 20px 0;
+    margin: 0 auto;
+
+    p {
+      font-size: 35px;
+      padding: 0px 0 20px 0;
       position: relative;
-      top:-20px;
+      top: -20px;
     }
-    img{width:80%;}
+
+    img {
+      width: 80%;
+    }
 
   }
 </style>
