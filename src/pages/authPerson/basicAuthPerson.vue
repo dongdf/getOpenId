1b05fcbb1a750925740a0be27e83e92f4c80471e
12366 <zhengwencheng@12366.com>
2019-11-04 13:17:50 +0800

    宿迁创业宝视频认证

--- src/pages/authPerson/basicAuthPerson.vue
+++ src/pages/authPerson/basicAuthPerson.vue
@@ -1,94 +1,94 @@
 <template>
-   <div class="aucontent">
-     <ul>
-<!--{{upimgzm}}-->
-       <li v-if="upimgzm">
-         <img :src="upimgzm" />
-         <span class="delicon" @click="delimgzm()">删除</span></li>
-       <li @click="chooseimgs(1)" v-else><img src="../../assets/img/authZm.jpg"/><wxupload ref="zm" @upinfozm="upinfozm"></wxupload></li>
-
-       <li v-if="upimgfm">
-         <img :src="upimgfm"/>
-         <span  class="delicon" @click="delimgfm()">删除</span></li>
-       <li  @click="chooseimgs(2)" v-else><img src="../../assets/img/authFm.jpg"/><wxupload ref="fm" @upinfofm="upinfofm"></wxupload></li>
-
-     </ul>
-     <div class="auDemo">
-       <img src="../../assets/img/authDemo.jpg"/>
-     </div>
-   </div>
+  <div class="aucontent">
+    <ul>
+      <li v-if="upimgzm">
+        <img :src="upimgzm"/>
+        <span class="delicon" @click="delimgzm()">删除</span></li>
+      <li @click="chooseimgs(1)" v-else><img src="../../assets/img/authZm.jpg"/>
+        <wxupload ref="zm" @upinfozm="upinfozm"></wxupload>
+      </li>
+      <li v-if="upimgfm">
+        <img :src="upimgfm"/>
+        <span class="delicon" @click="delimgfm()">删除</span></li>
+      <li @click="chooseimgs(2)" v-else><img src="../../assets/img/authFm.jpg"/>
+        <wxupload ref="fm" @upinfofm="upinfofm"></wxupload>
+      </li>
+    </ul>
+    <div class="auDemo">
+      <img src="../../assets/img/authDemo.jpg"/>
+    </div>
+  </div>
 </template>
 
 <script>
   import {IMG_URL} from '@/constants'
+
   export default {
-    name: "basicAuthPerson",
-    data(){
-      return{
-        upimgzm:'',
-        idcard:'',
-        upimgfm:''
+    name: 'basicAuthPerson',
+    data () {
+      return {
+        upimgzm: '',
+        idcard: '',
+        upimgfm: ''
       }
-
     },
-    methods:{
-      chooseimgs(str){
-        if(str == 1){
-          this.$refs.zm.chooseImg('1');
-        }else {
-          this.$refs.fm.chooseImg('2');
+    methods: {
+      chooseimgs (str) {
+        if (str === 1) {
+          this.$refs.zm.chooseImg('1')
+        } else {
+          this.$refs.fm.chooseImg('2')
         }
-
-
       },
-      upinfozm(value){
-
-        this.upimgzm = IMG_URL+value
-        // alert(this.upimgzm)
+      upinfozm (value) {
+        this.upimgzm = IMG_URL + value
       },
-      upinfofm(value){
-        // alert(value)
-        this.upimgfm = IMG_URL+value
+      upinfofm (value) {
+        this.upimgfm = IMG_URL + value
       },
-
-      delimgzm(){
-
-        this.upimgzm =''
+      delimgzm () {
+        this.upimgzm = ''
       },
-      delimgfm(){
-        this.upimgfm =''
+      delimgfm () {
+        this.upimgfm = ''
       }
-
     }
   }
 </script>
 
 <style lang="scss" scoped>
 
-  .aucontent{
-    ul{
-      padding:10px 30px;
-      li{
-        margin:10px 0;
+  .aucontent {
+    ul {
+      padding: 10px 30px;
+
+      li {
+        margin: 10px 0;
         position: relative;
 
-        img{
-          width:100%;
+        img {
+          width: 100%;
         }
 
       }
     }
   }
-  .auDemo{
-    padding:10px 30px;
-    img{width:100%;}
+
+  .auDemo {
+    padding: 10px 30px;
+
+    img {
+      width: 100%;
+    }
   }
-  .delicon{
+
+  .delicon {
     position: absolute;
-    top:-10px;
-    right:-10px;
+    top: -10px;
+    right: -10px;
     background: red;
-    color:#FFF;
-    padding:3px 10px; border-radius: 20px;
+    color: #FFF;
+    padding: 3px 10px;
+    border-radius: 20px;
   }
 </style>
